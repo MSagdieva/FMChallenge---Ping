d9585e95b6d02d31a6b7ddc9695ec457d56abf70
Mariya <grigorieva.ms@yandex.ru>
2021-09-25 20:24:54 +0300

    add scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,23 @@
 let form = document.querySelector(".subscribe-form");
 let email = document.querySelector(".email");
-
+let mes = document.querySelector(".error-message");
 
 
 function validateForm(){
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    if(!(validateEmail(email.value))){
+    if (!email.value){
+        mes.innerHTML = "Whoops! It looks like you forgot to add your email"
+        email.classList.add('error');
+    }
+    else
+    {if(!(validateEmail(email.value))){
+        mes.innerHTML = "Please provide a valid email"
         email.classList.add('error');
     }
     else{
         email.classList.remove('error');
-    }
+    }}
 })
 }
 
